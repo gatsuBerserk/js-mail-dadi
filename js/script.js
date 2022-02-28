442adc489707e5a4309ea21ff75f8a9af1cde9b9
gatsuBerserk <berserkdev@outlook.it>
2022-02-28 16:39:24 +0100

    correzioine esercizio

--- js/script.js
+++ js/script.js
@@ -1,34 +1,54 @@
 const mails= ["tool@mail.it", "storm@mail.com", "petrucci@mail.org"]
 console.log(mails);
-let found= document.getElementById("found");
-found.innerHTML="";
-const verifyButtom= document.getElementById("verify"); 
 
-verifyButtom.addEventListener("click", function(){   
-    const controlMail = document.getElementById("user-input").value; 
-    console.log(mails.includes(controlMail)); 
-    // console.log(controlMail); 
-    
-    for ( let i = 0 ; i < mails.length; i++) {
-        if (controlMail === (mails[i]) )  {  
-            found.append ("mail trovata");
-            console.log("mail trovata");
-        }
+const userMail= prompt("Inserisci la tua mail:");
+
+let  mailsRegistered= false; 
+
+for (let i = 0; i < mails.length; i++ ){
+    if ( mails[i] == userMail){
+        mailsRegistered= true;
     }
-}); 
+}; 
 
-
-
-
-const userNumber = Math.floor(Math.random() *6 +1);
-console.log(userNumber); 
-const pcNumber =  Math.floor(Math.random() *6 +1);
-console.log(pcNumber); 
-
-if ( userNumber > pcNumber ){
-    console.log("Umano vince ahahhahhahahahahahaha");
-}else if (userNumber < pcNumber){
-    console.log("CyberTron vince");
+if (mailsRegistered == true){
+    console.log("Email presente");
 }else{
-    console.log("Pareggio")
+    console.log("mail non presente");
 }
+
+
+
+
+
+
+
+
+
+
+
+const startGame=document.getElementById("start-game");
+
+startGame.addEventListener("click", function(){
+
+    const userNumber= Math.floor(Math.random() *6 +1);
+    document.getElementById("human-number").innerHTML= userNumber; 
+    console.log(userNumber); 
+
+    const pcNumber=innerHTML=Math.floor(Math.random() *6 +1); 
+    document.getElementById("cybertron-number").innerHTML= pcNumber; 
+    console.log(pcNumber); 
+
+    const resut=document.getElementById("result");
+
+    if ( userNumber > pcNumber ){
+        result.innerHTML="umano vince";
+        console.log("Umano vince ahahhahhahahahahahaha");
+    }else if (userNumber < pcNumber){ 
+        result.innerHTML="umano perde"; 
+        console.log("CyberTron vince");
+    }else{ 
+        result.innerHTML="nulla di fatto"; 
+        console.log("Pareggio");
+    }
+});
